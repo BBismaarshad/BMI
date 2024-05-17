4d1d9ef784cc07a9e40d26b1177aa2f381e2d846
BismaArshad <164316452+BBismaarshad@users.noreply.github.com>
2024-05-17 14:45:03 +0500

    push my BMI Calculater to github

---
+++ main.js
@@ -0,0 +1,38 @@
+//import inquirer from "inquirer"
+//const BMI = await inquirer.prompt([
+//  {
+//    type: "number",
+//  name: "weigth",
+//message: "kindly enter your weight"
+//},
+//
+//  {
+//     type: "number",
+//   name: "heigth",
+// message: "kindly enter your height"
+//},
+//]);
+//let w = BMI.weight
+//let h = BMI.heigth
+//console.log(` your body mass index is ${w/(h*h)}`)
+//console.log(BMI.weight);
+//.log(BMI.heigth);
+//console.log(` your body mass index is ${w / h * h}`);
+import inquier from "inquirer";
+const BMI = await inquier.prompt([
+    {
+        type: "number",
+        name: "weight",
+        message: " kindly enter your weight"
+    },
+    {
+        type: "number",
+        name: "hegiht",
+        message: "kindly enter your hegiht"
+    },
+]);
+let w = BMI.weight;
+let h = BMI.hegiht;
+console.log(`your body mass index is ${w / (h * h)}`);
+console.log(BMI.weight);
+console.log(BMI.hegiht);
